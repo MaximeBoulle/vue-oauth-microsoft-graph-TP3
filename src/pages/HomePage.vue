ee27ffa50dec4b5b3ba2b5540455a5a01cc4204b
Maxime Boulle <maxime.boulle1@gmail.com>
2024-09-20 16:36:31 +0200

    Ex 4: create HomePage component

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home-page">
+        <h1>Welcome to the Home Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomePage'
+}
+</script>
+
+<style scoped>
+.home-page {
+    text-align: center;
+    margin-top: 50px;
+}
+
+h1 {
+    color: #42b983;
+}
+
+p {
+    font-size: 18px;
+}
+</style>
